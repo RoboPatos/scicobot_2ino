5547b8c29094d57ddf74a3ccb784780c32249249
Natanael <amilnatanael@gmail.com>
2024-08-30 11:07:45 -0300

    att scicobot 2.0

--- scicobot_2/scicobot_2.ino
+++ scicobot_2/scicobot_2.ino
@@ -1,48 +1,80 @@
-//                       SciCoBot_2.0 
+//pinos utilizados para controlar a velocidade de rotação
+const int PINO_ENA = 6; 
+const int PINO_ENB = 5;
 
-//A ideia aqui é receber dois valores via serial e controlar os motores isoladamente
-//Esse projeto foi pensado para se comunicar com um raspberry por meio de nós ros2. 
+//pinos utilizados para controlar o sentido do motor
+const int PINO_IN1 = 4; 
+const int PINO_IN2 = 3;
+const int PINO_IN3 = 8;
+const int PINO_IN4 = 7;
 
-//////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////  TESTE ////////////////////////////////////////////
+int speed1;
+int speed2;
+int motor1PWM;
+int motor2PWM;
 
-// Definição dos pinos para os motores
-const int PINO_ENA = 6; //pino utilizado para controlar a velocidade do motor A 
-const int PINO_ENB = 5; //pino utilizado para controlar a velocidade do motor B
+char rdata; // received charactors
+String rStr = "";
 
-const int PINO_IN1 = 4; //pino utilizado para controlar o sentido do motor A
-const int PINO_IN2 = 3; //pino utilizado para controlar o sentido do motor A
-const int PINO_IN3 = 8; //pino utilizado para controlar o sentido do motor B
-const int PINO_IN4 = 7; //pino utilizado para controlar o sentido do motor B
+int params[4];
+
+void run_motors();
 
 void setup() {
-  
-  // Configura os pinos como saída
-  pinMode(PINO_ENA, OUTPUT); // Configura os pinos como saída
-  pinMode(PINO_ENB, OUTPUT); // Configura os pinos como saída
-  pinMode(PINO_IN1, OUTPUT); // Configura os pinos como saída
-  pinMode(PINO_IN2, OUTPUT); // Configura os pinos como saída
-  pinMode(PINO_IN3, OUTPUT); // Configura os pinos como saída
-  pinMode(PINO_IN4, OUTPUT); // Configura os pinos como saída
+  Serial.begin(9600);
 
-    Serial.begin(9600); // Inicializa a comunicação serial
+  //configuração dos pinos como saida
+  pinMode(PINO_ENA, OUTPUT); 
+  pinMode(PINO_ENB, OUTPUT);
+  pinMode(PINO_IN1, OUTPUT);
+  pinMode(PINO_IN2, OUTPUT);
+  pinMode(PINO_IN3, OUTPUT);
+  pinMode(PINO_IN4, OUTPUT);
+
+  //inicia o codigo com os motores parados
+  digitalWrite(PINO_IN1, LOW); 
+  digitalWrite(PINO_IN2, LOW);
+  digitalWrite(PINO_IN3, LOW);
+  digitalWrite(PINO_IN4, LOW);
+  digitalWrite(PINO_ENA, LOW);
+  digitalWrite(PINO_ENB, LOW);
 }
 
 void loop() {
+   if (Serial.available()) {
+    rdata = Serial.read();
+    rStr = rStr + rdata;
+    if( rdata == '\n')
+    { 
+      if(rStr.substring(0, 2).equals("o ")) 
+      {
+        int sepIndex = rStr.indexOf(" ", 2); // começando em 2 pq "o " ocupam 0 e 1
+        speed1 = rStr.substring(2, sepIndex).toInt();
+        Serial.print("speed1 ");
+        Serial.println(speed1);
+        speed2 = rStr.substring(sepIndex+1).toInt();
+        Serial.print("speed2 ");
+        Serial.println(speed2);
+        
+        motor1PWM = abs(speed1); //módulo para definição da velocidade do motor A
+        motor2PWM = abs(speed2); //módulo para definição da velocidade do motor B
 
-    if (Serial.available() >= 2) {// Verifica se há dados disponíveis na serial
-    int motor1Speed = Serial.read();  // Lê os valores enviados pela serial
-    int motor2Speed = Serial.read();  // Lê os valores enviados pela serial
+        Serial.print("motor1PWM ");
+        Serial.println(motor1PWM);
+        Serial.print("motor2PWM ");
+        Serial.println(motor2PWM);
+        
+        run_motors();
+      }
 
-    // Mapeia os valores de -255 a 255 para 0 a 255 para PWM
-    int motor1PWM = abs(motor1Speed); //módulo para definição da velocidade do motor A
-    int motor2PWM = abs(motor2Speed); //módulo para definição da velocidade do motor B
+      rStr = "";
 
-//                        Até aqui BELEZA
+    }
+  }
+}
 
-
-    // Controle do Motor 1
-    if (motor1Speed >= 0) { //Sentido Horario
+void run_motors(){
+  if (speed1 >= 0) { //Sentido Horario
         digitalWrite(PINO_IN1, LOW); //Direção motor 1
         digitalWrite(PINO_IN2, HIGH); //Direção motor 1
         analogWrite(PINO_ENA, motor1PWM); // Velocidade motor 1
@@ -53,7 +85,7 @@ void loop() {
     }
 
     // Controle do Motor 2
-    if (motor2Speed >= 0) {
+    if (speed2 >= 0) {
         digitalWrite(PINO_IN3, LOW); //Direção motor 2
         digitalWrite(PINO_IN4, HIGH); //Direção motor 2
         analogWrite(PINO_ENB, motor2PWM); // Velocidade motor 2
@@ -62,5 +94,4 @@ void loop() {
         digitalWrite(PINO_IN4, LOW); //Direção motor 2
         analogWrite(PINO_ENB, motor2PWM); // Velocidade motor 2
     }
-  }
 }
